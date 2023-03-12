e72ff0c63ada6fd1c320e09bc369fb094d75f4e8
Javier Chiple <javichiple@gmail.com>
2023-03-12 19:18:39 -0300

    Fin de la seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,19 +10,37 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada: boolean  = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient) { 
 
-     console.log('Servicio de infor pagina listo');
+     //console.log('Servicio de infor pagina listo');
 
-     //Leer el archivo JSON
-     this.http.get ('assets/data/data-pagina.json')
-        .subscribe ((resp: InfoPagina) => { //de la peticion http vamos a recibir una respuesta.
-          console.log(resp);
+     this.cargarInfo();
+     this.cargarEquipo();
+     
 
-          this.cargada = true;
-          this.info = resp;
+   }
 
-        })
+   cargarInfo(){
+    //Leer el archivo JSON
+    this.http.get ('assets/data/data-pagina.json')
+    .subscribe ((resp: InfoPagina) => { //de la peticion http vamos a recibir una respuesta.
+      //console.log(resp);
 
+      this.cargada = true;
+      this.info = resp;
+
+    })
+   }
+
+   cargarEquipo(){
+
+    this.http.get('https://miportafolio-c692c-default-rtdb.firebaseio.com/equipo.json')
+      .subscribe((equipo:any) => {
+
+        this.equipo = equipo;
+        //console.log (equipo);
+      })
    }
 }
